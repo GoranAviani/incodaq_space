43b0be299a4e5df16ff2c646be1932e4d790fdd5
goranaviani <goranaviani@gmail.com>
2020-02-01 16:47:26 +0100

    Added iss web page

--- incodaq_space/incodaq_space/views.py
+++ incodaq_space/incodaq_space/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from iss.views import iss_crew_api
 
 def index(request):
-    iss_crew_api()
 
     return render(request,'index.html')
+
+
+def iss(request):
+    iss_crew_api()
+    return render(request, 'iss.html')
