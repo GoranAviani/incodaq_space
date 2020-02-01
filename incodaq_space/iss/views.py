93eeea6f401e6a83ea0c2a05c5a72a0ebec23f90
goranaviani <goranaviani@gmail.com>
2020-02-01 17:55:02 +0100

    saving iss crew data to iss_crew model

--- incodaq_space/iss/views.py
+++ incodaq_space/iss/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from api_relay.make_requests import make_iss_api_call
 # Create your views here.
-
+from .models import iss_crew
 def iss_crew_api():
     call_source = {"call_source": "iss_crew_data"}
     iss_crew_data = make_iss_api_call(**call_source)
     print(iss_crew_data)
+
+    #save data to iss_crew model
+    for x in iss_crew_data["people"]:
+        iss_crew.objects.create(name=x["name"], craft=x["craft"])
+
     #TODO process data from make_iss_api_call?
