7b88118ff19876bf93a1ef86e83f9bf78bb4a103
goranaviani <goranaviani@gmail.com>
2020-02-01 17:18:36 +0100

    iss crew model

--- incodaq_space/iss/models.py
+++ incodaq_space/iss/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class iss_crew(models.Model):
+    name = models.CharField(max_length=150, null=True, blank=True)
+    craft = models.CharField(max_length=50, null=True, blank=True)
+    datetime = models.DateTimeField(default=datetime.datetime.utcnow)
