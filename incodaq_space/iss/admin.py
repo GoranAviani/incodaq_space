524980ed42d990948aa289d426c5590c991f8e4f
goranaviani <goranaviani@gmail.com>
2020-02-01 18:42:52 +0100

    added iss_crew_model to admins

--- incodaq_space/iss/admin.py
+++ incodaq_space/iss/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import iss_crew
+from .models import iss_crew, iss_crew_model
 
-class iss_crew_admin (admin.ModelAdmin):
-    model = iss_crew
-    list_display = ("name", "craft", "datetime")
+class iss_crew_model_admin (admin.ModelAdmin):
+    model = iss_crew_model
+    list_display = ("iss_crew_json")
 
-admin.site.register(iss_crew, iss_crew_admin)
+admin.site.register(iss_crew_model)
