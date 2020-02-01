a3affd87057f9091e55284fa54f1307eb4e8c718
goranaviani <goranaviani@gmail.com>
2020-02-01 17:23:30 +0100

    iss crew info is now displayed to admins

--- incodaq_space/iss/admin.py
+++ incodaq_space/iss/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import iss_crew
+
+class iss_crew_admin (admin.ModelAdmin):
+    model = iss_crew
+    list_display = ("name", "craft", "datetime")
+
+admin.site.register(iss_crew, iss_crew_admin)
