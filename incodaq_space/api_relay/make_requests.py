66a291f9386d8b498e0def4534ef90c816cef7f5
goranaviani <goranaviani@gmail.com>
2020-02-02 17:41:11 +0100

    Renaming iss_crew_api vars

--- incodaq_space/api_relay/make_requests.py
+++ incodaq_space/api_relay/make_requests.py
@@ -8,7 +8,7 @@ def make_iss_api_call(**kwargs):
         #TODO log failure to retrieve call_source
         #return error message
 
-    if call_source == "iss_crew_data":
+    if call_source == "iss_crew_names":
         try:
             result = requests.get("http://api.open-notify.org/astros.json")
         except:
