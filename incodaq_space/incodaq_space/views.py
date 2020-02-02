522e1bfae4880d0edd27b76cad1575a61191065f
goranaviani <goranaviani@gmail.com>
2020-02-02 18:08:58 +0100

    Retrieving last record of ISS location

--- incodaq_space/incodaq_space/views.py
+++ incodaq_space/incodaq_space/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from iss.views import iss_crew_api
-from iss.models import iss_crew_model
+from iss.models import iss_crew_model, iss_location_now_model
 import json
 #from datetime import datetime, timedelta
 
@@ -13,8 +13,18 @@ def get_iss_crew_info():
 
       return IssCrewData
    except:
-      return [{'name': "Something went wrong with fetching crew data", "craft": ""}]
+      return [{'name': "Something went wrong with fetching ISS crew data", "craft": ""}]
 
+def get_iss_location_now_info():
+   try:
+      lastISSLocationDataRec = iss_location_now_model.objects.last()
+      lastISSLocationDataRec = json.loads(lastISSLocationDataRec.iss_location_now_json)
+      IssLocationData = lastISSLocationDataRec
+
+
+      return IssLocationData
+   except:
+      return [{'name': "Something went wrong with fetching ISS location data", "craft": ""}]
 def index(request):
 
     return render(request,'index.html')
@@ -24,7 +34,10 @@ def iss(request):
     #iss_crew_api() #TODO to be moved to clery
 
     iss_crew_info = get_iss_crew_info()
+    iss_location_now_info = get_iss_location_now_info()
     print(iss_crew_info)
+    print(iss_location_now_info)
     return render(request, 'iss.html',
-            {'iss_crew_info': iss_crew_info,}
+            {'iss_crew_info': iss_crew_info,
+             "iss_location_now_info": iss_location_now_info}
             )
