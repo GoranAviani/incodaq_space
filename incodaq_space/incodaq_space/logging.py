e224735644298a15872102441607a5b7c1214629
goranaviani <goranaviani@gmail.com>
2020-03-31 16:49:30 +0200

    Created first logger handlers

--- incodaq_space/incodaq_space/logging.py
+++ incodaq_space/incodaq_space/logging.py
@@ -1,4 +1,18 @@
 import logging
 from datetime import datetime
 
+#Custom loggers
+api_errors = logging.getLogger("api_error")
 
+#Handlers
+api_errors_handler = logging.FileHandler('api_errors.log')
+
+#Handlers level
+api_errors_handler.setLevel(logging.INFO)
+
+#Create formatters and add them to handlers
+error_format = logging.Formatter('%(asctime)s - %(message)s')
+api_errors_handler.setFormatter(error_format)
+
+#Connect handlers and loggers
+api_errors.addHandler(api_errors_handler)
