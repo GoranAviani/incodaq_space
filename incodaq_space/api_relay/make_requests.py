376f1507c28a726e88e27e92fabc1ffae622923e
goranaviani <goranaviani@gmail.com>
2020-03-31 16:25:55 +0200

    api error handling

--- incodaq_space/api_relay/make_requests.py
+++ incodaq_space/api_relay/make_requests.py
@@ -21,18 +21,20 @@ def make_iss_api_call(**kwargs):
         except requests.exceptions.RequestException as e:
             return "error", "Other error"
             # catastrophic error. bail.
-        try:
-            result.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            # Whoops it wasn't a 200
-            return result.status_code, {'name': "Error: " + str(e)} #TODO fix json
 
     elif call_source == "iss_location_now":
-        result = requests.get("http://api.open-notify.org/iss-now.json")
         try:
+            result = requests.get("http://api.open-notify.org/iss-now.json")
             result.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            # Whoops it wasn't a 200
-            return result.status_code, {'Error message': str(e)} #TODO fix json
+        except requests.exceptions.Timeout:
+        # Maybe set up for a retry, or continue in a retry loop
+            return "error", "The call had timeout"
+        except requests.exceptions.TooManyRedirects:
+        # Tell the user their URL was bad and try a different one
+            return "error", "Too many redirects"
+        except requests.exceptions.RequestException as e:
+            return "error", "Other error"
+            # catastrophic error. bail.
+
 
     return result.status_code, result.json()
