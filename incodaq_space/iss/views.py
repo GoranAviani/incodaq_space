7818d575845d898e93c23f072086a3ae728e7ee7
goranaviani <goranaviani@gmail.com>
2020-08-17 19:18:45 +0200

    making new api calls

--- incodaq_space/iss/views.py
+++ incodaq_space/iss/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
-from api_relay.make_requests import retrieve_iss_crew_names, retrieve_iss_location_now
+from api_relay.make_requests import fetch_iss_crew_names, fetch_iss_location
 # Create your views here.
 from .models import iss_crew_model, iss_location_now_model
 import json
 
 def iss_crew_api():
-    iss_iss_crew_api_response = retrieve_iss_crew_names()
+    iss_iss_crew_api_response = fetch_iss_crew_names()
     iss_crew_data = json.dumps(iss_iss_crew_api_response.json()) #save json as string
     #save data to iss_crew model
     iss_crew_model.objects.create(iss_crew_json=iss_crew_data)
 
 def iss_location_api():
-    iss_location_api_response = retrieve_iss_location_now()
+    iss_location_api_response = fetch_iss_location()
     iss_locatio_data = json.dumps(iss_location_api_response.json()) #save json as string
     #save data to iss_crew model
     iss_location_now_model.objects.create(iss_location_now_json= iss_locatio_data)
