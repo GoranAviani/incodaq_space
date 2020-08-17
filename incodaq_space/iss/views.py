6442740cf6bf49ea28bc98b20e3edf73d45b1210
goranaviani <goranaviani@gmail.com>
2020-08-17 18:58:15 +0200

    renaming vars

--- incodaq_space/iss/views.py
+++ incodaq_space/iss/views.py
@@ -5,15 +5,15 @@ from .models import iss_crew_model, iss_location_now_model
 import json
 
 def iss_crew_api():
-    callSource = {"call_source": "iss_crew_names"}
-    issCrewDataResult = make_iss_api_call(**callSource)
-    issCrewDataString = json.dumps(issCrewDataResult.json()) #save json as string
+    iss_crew_parameters = {"call_source": "iss_crew_names"}
+    iss_iss_crew_api_response = make_iss_api_call(**iss_crew_parameters)
+    iss_crew_data = json.dumps(iss_iss_crew_api_response.json()) #save json as string
     #save data to iss_crew model
-    iss_crew_model.objects.create(iss_crew_json=issCrewDataString)
+    iss_crew_model.objects.create(iss_crew_json=iss_crew_data)
 
 def iss_location_api():
-    callSource = {"call_source": "iss_location_now"}
-    issLocationNowResult = make_iss_api_call(**callSource)
+    iss_location_parameters = {"call_source": "iss_location_now"}
+    issLocationNowResult = make_iss_api_call(**iss_location_parameters)
     issLocationNowString = json.dumps(issLocationNowResult.json()) #save json as string
     #save data to iss_crew model
     iss_location_now_model.objects.create(iss_location_now_json= issLocationNowString)
