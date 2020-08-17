570c862369acdc6ffe843aa1cfe37798e642a840
goranaviani <goranaviani@gmail.com>
2020-08-17 19:09:41 +0200

    making new api calls

--- incodaq_space/api_relay/make_requests.py
+++ incodaq_space/api_relay/make_requests.py
@@ -29,28 +29,28 @@ def fetch_iss_crew_names():
                                                            result.json()))
         return result
 
-def retrieve_iss_location_now():
+def fetch_iss_location():
     try:
-        api_logs.info("Api request: {}: Url: {}".format("Function fetch_iss_crew_names", ASTRONAUTS_IN_SPACE_URL))
+        api_logs.info("Api request: {}: Url: {}".format("Function fetch_iss_location", ASTRONAUTS_IN_SPACE_URL))
         result = requests.get(ISS_LOCATION_URL)
         result.raise_for_status()
     except requests.exceptions.HTTPError as e:
         status_code = result.status_code
         api_errors.error(("status code: {},error: {}, Api request: {}: Url: {}".format(status_code, e,
-                                                                                       "Function fetch_iss_crew_names",
+                                                                                       "Function fetch_iss_location",
                                                                                        ASTRONAUTS_IN_SPACE_URL)))
         api_logs.error(("status code: {},error: {}, Api request: {}: Url: {}".format(status_code, e,
-                                                                                       "Function fetch_iss_crew_names",
+                                                                                       "Function fetch_iss_location",
                                                                                        ASTRONAUTS_IN_SPACE_URL)))
     except requests.exceptions.RequestException as e:
         api_errors.error(("Error: {}, Api request: {}: Url: {}".format(e,
-                                                                       "Function retrieve_iss_crew_names",
+                                                                       "Function fetch_iss_location",
                                                                        ASTRONAUTS_IN_SPACE_URL)))
         api_logs.error(("Error: {}, Api request: {}: Url: {}".format(e,
-                                                                       "Function retrieve_iss_crew_names",
+                                                                       "Function fetch_iss_location",
                                                                        ASTRONAUTS_IN_SPACE_URL)))
     else:
         api_logs.info(
-            "Api response: {}: Url: {}, result: {}".format("Function retrieve_iss_location_now", ASTRONAUTS_IN_SPACE_URL,
+            "Api response: {}: Url: {}, result: {}".format("Function fetch_iss_location", ASTRONAUTS_IN_SPACE_URL,
                                                            result.json()))
         return result
