64acb7fa1fe267e298c004cd65f38a690a8473ca
goranaviani <goranaviani@gmail.com>
2020-08-19 16:39:38 +0200

    refactoring api calls by making a uniques process_api_response function that processes all api answers at one olace

--- incodaq_space/api_relay/make_requests.py
+++ incodaq_space/api_relay/make_requests.py
@@ -38,27 +38,9 @@ def fetch_iss_crew_names():
     return result
 
 def fetch_iss_location():
-    try:
-        api_logs.info("Api request: {}: Url: {}".format("Function fetch_iss_location", ASTRONAUTS_IN_SPACE_URL))
-        result = requests.get(ISS_LOCATION_URL)
-        result.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        status_code = result.status_code
-        api_errors.error(("status code: {},error: {}, Api request: {}: Url: {}".format(status_code, e,
-                                                                                       "Function fetch_iss_location",
-                                                                                       ASTRONAUTS_IN_SPACE_URL)))
-        api_logs.error(("status code: {},error: {}, Api request: {}: Url: {}".format(status_code, e,
-                                                                                       "Function fetch_iss_location",
-                                                                                       ASTRONAUTS_IN_SPACE_URL)))
-    except requests.exceptions.RequestException as e:
-        api_errors.error(("Error: {}, Api request: {}: Url: {}".format(e,
-                                                                       "Function fetch_iss_location",
-                                                                       ASTRONAUTS_IN_SPACE_URL)))
-        api_logs.error(("Error: {}, Api request: {}: Url: {}".format(e,
-                                                                       "Function fetch_iss_location",
-                                                                       ASTRONAUTS_IN_SPACE_URL)))
-    else:
-        api_logs.info(
-            "Api response: {}: Url: {}, result: {}".format("Function fetch_iss_location", ASTRONAUTS_IN_SPACE_URL,
-                                                           result.json()))
-        return result
+    name_of_call_function = 'fetch_iss_location'
+    api_logs.info("Api request: {}: Url: {}".format("Function fetch_iss_location", ASTRONAUTS_IN_SPACE_URL))
+    result = requests.get(ISS_LOCATION_URL)
+    result = process_api_response(name_of_call_function, result, ISS_LOCATION_URL)
+    
+    return result
