8412f56a8a28b4f682fac263247a32725f210401
goranaviani <goranaviani@gmail.com>
2020-08-22 11:59:46 +0200

    asteroids model

--- incodaq_space/asteroids/models.py
+++ incodaq_space/asteroids/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class asteroids_location_model(models.Model):
+    asteroids_location_json = models.CharField(max_length=15000, null=True, blank=True)
+    datetime = models.DateTimeField(default=datetime.datetime.utcnow)
