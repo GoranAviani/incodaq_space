6752a8bfb831982c8d358cfec12ae1e36fc9c9ff
goranaviani <goranaviani@gmail.com>
2020-08-27 16:58:56 +0200

    displaying detailed asteroid info quickfix

--- incodaq_space/asteroids/processing_asteroid_data.py
+++ incodaq_space/asteroids/processing_asteroid_data.py
@@ -2,9 +2,9 @@
 def process_asteroid_info(asteroid_info):
     test = asteroid_info
     test = asteroid_info
-    for date, list_of_asteroids in asteroid_info.items():
+    for date, list_of_asteroids in asteroid_info['near_earth_objects'].items():
         print("############################################################3")
         print(date)
-        for asteroid in list_of_asteroids['near_earth_objects']:
+        for asteroid in list_of_asteroids:
             print("******************************")
             print(asteroid)
